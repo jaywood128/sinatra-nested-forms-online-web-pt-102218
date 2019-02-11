91bb1ede7ebf48bb14b08e23800738978af33877
Johnathon Wood <jaywood128@gmail.com>
2019-02-11 18:11:56 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -20,6 +20,8 @@ module FormsLab
 
      @ship_1 = Ship.new(params["pirate"]["ships"][0]["name"], params["pirate"]["ships"][0]["type"], params["pirate"]["ships"][0]["booty"])
 
+     @ship_2 = Ship.new(params["pirate"]["ships"][1]["name"], params["pirate"]["ships"][1]["type"], params["pirate"]["ships"][1]["booty"])
+
     erb :'pirates/show'
   end
 end
