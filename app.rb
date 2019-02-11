10c817ba43393e922434fe0590183c2efbc60e53
Johnathon Wood <jaywood128@gmail.com>
2019-02-11 17:56:51 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -17,7 +17,7 @@ module FormsLab
   post '/pirates' do
 
      @pirate = Pirate.new(params["pirate"]["name"], params["pirate"]["weight"], params["pirate"]["height"])
-    
+
      @ship_1 = Ship.new(params["pirate"]["ships"][0]["name"], params["pirate"]["ships"][0]["type"], params["pirate"]["ships"][0]["booty"])
     erb :'pirates/show'
   end
